43ba9fc62ff945ccc833bd9548d3415e205b17c6
Prathinav-kV <prathinav.kv@gmail.com>
2024-11-23 22:09:57 -0500

    Added a replay_world.launch.py file
    Added the replay_world.launch.py launch file to test out the ros2 bag recording.

---
+++ launch/replay_world.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Launch Gazebo with the custom world
+        ExecuteProcess(
+            cmd=[
+                'gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',
+                '/home/pratkv/walker_ws/src/walker/worlds/newest_2.world'
+            ],
+            output='screen'
+        ),
+
+        # Spawn the TurtleBot3 in the custom world
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'turtlebot3_burger',
+                '-file', '/opt/ros/humble/share/turtlebot3_description/urdf/turtlebot3_burger.urdf',
+                '-x', '0', '-y', '0', '-z', '0'
+            ],
+            output='screen'
+        ),
+    ])
